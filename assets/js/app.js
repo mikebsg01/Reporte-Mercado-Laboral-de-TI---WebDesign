b80056f0cb4c4927dfc5936326c7d7e8d4a44e32
Michael Serrato <mikebsg01@gmail.com>
2018-02-16 14:48:33 -0600

    Add Doughnut Charts - No.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,13 +16,13 @@ $(document).ready(() => {
             3.2
           ],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.4)',
-            'rgba(54, 162, 235, 0.4)',
-            'rgba(255, 206, 86, 0.4)',
-            'rgba(75, 192, 192, 0.4)',
-            'rgba(153, 102, 255, 0.4)',
-            'rgba(255, 159, 64, 0.4)',
-            'rgba(155, 125, 175, 0.4)'
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 159, 64, 0.5)',
+            'rgba(155, 125, 175, 0.5)'
           ],
           borderColor: [
             'rgba(255,99,132,1)',
@@ -54,7 +54,7 @@ $(document).ready(() => {
             fontFamily: "'Ubuntu', sans-serif",
             fontColor: "#f1f1f1",
             fontSize: 12,
-            padding: 13
+            padding: 12
           }
         },
         title: {
@@ -73,67 +73,45 @@ $(document).ready(() => {
     }
   ); /* <-- End 'chartPointsScored' */
 
-  var chartStatesWithMoreGrowth = new Chart(
-    document.getElementById('chart-states-with-more-growth').getContext('2d'), 
-    { type: 'bar',
+  var chartGrowthInMexicoCity = new Chart(
+    document.getElementById('chart-growth-in-mexico-city').getContext('2d'), 
+    { type: 'doughnut',
       data: {
         datasets: [{
-            label: 'Ciudad de México',
-            data: [
-              59.53
-            ],
-            backgroundColor: 'rgba(255, 99, 132, 0.4)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1
-          },
-          {
-            label: 'Estado de México',
-            data: [
-              3.70
-            ],
-            backgroundColor: 'rgba(54, 162, 235, 0.4)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1
-          },
-          {
-            label: 'Querétaro',
-            data: [
-              6.87
-            ],
-            backgroundColor: 'rgba(255, 206, 86, 0.4)',
-            borderColor: 'rgba(255, 206, 86, 1)',
-            borderWidth: 1
-          },
-          {
-            label: 'Nuevo León',
-            data: [
-              10.29,
-            ],
-            backgroundColor: 'rgba(75, 192, 192, 0.4)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-          },
-          {
-            label: 'Jalisco',
-            data: [
-              11.54
-            ],
-            backgroundColor: 'rgba(153, 102, 255, 0.4)',
-            borderColor: 'rgba(153, 102, 255, 1)',
-            borderWidth: 1
-          }
+          data: [
+            59.53,
+            40.47
+          ],
+          backgroundColor: [
+            'rgba(74, 182, 255, 0.4)',
+            'rgba(222, 224, 226, 0.1)'
+          ],
+          borderColor: [
+            'rgba(74, 182, 255, 1)',
+            'rgba(222, 224, 226, 0.4)'
+          ],
+          borderWidth: 1,
+        }],
+        labels: [
+          'Ciudad de México',
+          '',
         ]
       },
       options: {
+        cutoutPercentage: 80,
         responsive: true,
         legend: {
+          display: false,
           position: 'top',
           labels: {
             fontFamily: "'Ubuntu', sans-serif",
             fontColor: "#f1f1f1",
             fontSize: 12,
-            padding: 18
+            padding: 13
           }
+        },
+        tooltips: {
+          enabled: false
         },
         animation: {
           animateScale: true,
@@ -141,7 +119,55 @@ $(document).ready(() => {
         }
       }
     }
-  ); /* <-- End 'chartStatesWithMoreGrowth' */
+  ); /* <-- End 'chartGrowthInMexicoCity' */
+
+  var chartGrowthInTheStateOfMexico = new Chart(
+    document.getElementById('chart-growth-in-the-state-of-mexico').getContext('2d'), 
+    { type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [
+            3.7,
+            96.3
+          ],
+          backgroundColor: [
+            'rgba(74, 182, 255, 0.4)',
+            'rgba(222, 224, 226, 0.1)'
+          ],
+          borderColor: [
+            'rgba(74, 182, 255, 1)',
+            'rgba(222, 224, 226, 0.4)'
+          ],
+          borderWidth: 1,
+        }],
+        labels: [
+          'Estado de México',
+          '',
+        ]
+      },
+      options: {
+        cutoutPercentage: 80,
+        responsive: true,
+        legend: {
+          display: false,
+          position: 'top',
+          labels: {
+            fontFamily: "'Ubuntu', sans-serif",
+            fontColor: "#f1f1f1",
+            fontSize: 12,
+            padding: 13
+          }
+        },
+        tooltips: {
+          enabled: false
+        },
+        animation: {
+          animateScale: true,
+          animateRotate: true
+        }
+      }
+    }
+  ); /* <-- End 'chartGrowthInTheStateOfMexico' */
 
   var chartMostDemandedProfiles = new Chart(
     document.getElementById('chart-most-demanded-profiles').getContext('2d'), 
