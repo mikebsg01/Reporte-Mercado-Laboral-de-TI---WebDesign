83d8805b31e42ad6fb9609ab182175cc09bcd51b
Michael Serrato <mikebsg01@gmail.com>
2018-03-13 12:41:40 -0600

    [CSS] 'gulp-sass' added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp'),
       babel = require('gulp-babel'),
       concat = require('gulp-concat'),
+      sass = require('gulp-sass'),
       uglify = require('gulp-uglify');
 
+gulp.task('minifyCSS', () => {
+  return gulp
+    .src('./assets/css/sass-project/main.scss')
+    .pipe(concat('style.css'))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./assets/css'))
+});
 
 gulp.task('minifyLibrariesJS', () => {
   return gulp
@@ -38,6 +46,20 @@ gulp.task('minifyAppJS', () => {
 });
 
 gulp.task('default', [
+  'minifyCSS',
   'minifyLibrariesJS',
   'minifyAppJS'
 ]);
+
+gulp.task('watch', () => {
+  gulp.watch('./assets/css/sass-project/main.scss', ['minifyCSS'])
+
+  gulp.watch([
+    './assets/js/jquery-3.2.1.min.js',
+    './assets/js/materialize.min.js',
+    './assets/js/scrollspy.min.js',
+    './assets/js/Chart.min.js',
+  ], ['minifyLibrariesJS']);
+
+  gulp.watch('./assets/js/app.js', ['minifyAppJS'])
+});
